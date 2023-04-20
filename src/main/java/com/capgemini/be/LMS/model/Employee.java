b0fb0a8fe79ec18f5ac24ad39ac7fe793175a633
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-04-20 14:33:50 +0200

    Employee class aangemaakt

---
+++ src/main/java/com/capgemini/be/LMS/model/Employee.java
@@ -0,0 +1,25 @@
+package com.capgemini.be.LMS.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private final List<LeaveRequest> leaveRequests;
+
+    public Employee(int id, String firstName, String lastName, String email, String phoneNumber, String jobTitle, String department) {
+        this.leaveRequests = new ArrayList<>();
+    }
+
+    public void addLeaveRequest(LeaveRequest leaveRequest) {
+        this.leaveRequests.add(leaveRequest);
+    }
+
+    public void removeLeaveRequest(LeaveRequest leaveRequest) {
+        this.leaveRequests.remove(leaveRequest);
+    }
+
+    public List<LeaveRequest> getLeaveRequests() {
+        return leaveRequests;
+    }
+}
+
