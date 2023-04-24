4a072ab397cdfe5779fe272adef7418858b09ea5
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-04-24 11:43:59 +0200

    lege arraylist aangemaakt

--- src/main/java/com/capgemini/be/service/LeaveRequestService.java
+++ src/main/java/com/capgemini/be/service/LeaveRequestService.java
@@ -3,12 +3,18 @@ package com.capgemini.be.service;
 import com.capgemini.be.LMS.model.LeaveRequest;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
 
 @ApplicationScoped
 public class LeaveRequestService {
 
     public LeaveRequest createLeaveRequest(LeaveRequest leaveRequest) {
+        // Lege ArrayList
+        ArrayList<String> myList = new ArrayList<String>();
+
         return leaveRequest;
     }
-}
 
+    public LeaveRequestService() {
+    }
+}
