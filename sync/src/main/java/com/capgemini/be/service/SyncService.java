5ce5a963d7bc9cea7b6ee258068b43ac2e76da5b
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-04-27 17:23:05 +0200

    Syncservice aangemaakt

--- sync/src/main/java/com/capgemini/be/service/SyncService.java
+++ sync/src/main/java/com/capgemini/be/service/SyncService.java
@@ -1,52 +1,37 @@
 package com.capgemini.be.service;
 
 import com.capgemini.be.lms.model.LeaveRequest;
-import jakarta.enterprise.context.ApplicationScoped;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
+import com.capgemini.be.proxy.SyncProxy;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-
-import java.io.IOException;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class SyncService {
+
+    private static final Logger LOGGER = Logger.getLogger(SyncService.class.getName());
+
+    @Inject
+    @RestClient
+    SyncProxy syncProxy;
 
     public LeaveRequest createLeaveRequest(LeaveRequest leaveRequest) {
         // create the leave request in the local database
         // ...
 
         // send the leave request to the sync application
-        String syncUrl = "http://localhost:8080/sync/leaves"; // replace with the actual URL of the sync application
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(syncUrl);
-
         try {
-            StringEntity requestBody = new StringEntity(
-                    "{\n" +
-                            "    \"localEmployeeId\": " + leaveRequest.getLocalEmployeeId() + ",\n" +
-                            "    \"leaveType\": \"" + leaveRequest.getLeaveType() + "\",\n" +
-                            "    \"startDate\": \"" + leaveRequest.getStartDate() + "\",\n" +
-                            "    \"endDate\": \"" + leaveRequest.getEndDate() + "\",\n" +
-                            "    \"startHours\": \"" + leaveRequest.getStartHours() + "\",\n" +
-                            "    \"endDateHours\": \"" + leaveRequest.getEndDateHours() + "\",\n" +
-                            "    \"totalHours\": \"" + leaveRequest.getTotalHours() + "\",\n" +
-                            "    \"reason\": \"" + leaveRequest.getReason() + "\",\n" +
-                            "    \"status\": \"" + leaveRequest.getStatus() + "\"\n" +
-                            "}"
-            );
-            post.setEntity(requestBody);
-            post.setHeader("Content-type", "application/json");
-
-            HttpResponse response = httpClient.execute(post);
-
-            if (response.getStatusLine().getStatusCode() != 200) {
-                throw new RuntimeException("Failed to sync leave request. Response code: " + response.getStatusLine().getStatusCode());
+            Response response = syncProxy.syncLeaveRequest(leaveRequest);
+            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+                throw new WebApplicationException("Failed to sync leave request. Response code: " + response.getStatus());
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to sync leave request: " + e.getMessage(), e);
+        } catch (Exception e) {
+            LOGGER.severe("Failed to sync leave request: " + e.getMessage());
+            throw new WebApplicationException("Failed to sync leave request. Reason: " + e.getMessage());
         }
 
         return leaveRequest;
