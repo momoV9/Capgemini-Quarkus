751a89e81c4460177376d4b254d8c390e407cf55
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-04-27 17:22:49 +0200

    SyncProxy aangemaakt

---
+++ sync/src/main/java/com/capgemini/be/proxy/SyncProxy.java
@@ -0,0 +1,24 @@
+package com.capgemini.be.proxy;
+
+import com.capgemini.be.lms.model.LeaveRequest;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/sync")
+@Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient
+public interface SyncProxy {
+
+    @POST
+    @Path("/leaves")
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response syncLeaveRequest(LeaveRequest leaveRequest);
+
+}
