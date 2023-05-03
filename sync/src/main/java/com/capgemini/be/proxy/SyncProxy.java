b943cc72886ca6cfc0065ed6f248d9ff0758dd3d
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-05-03 15:20:52 +0200

    Probleem opgelost sync wel nog in commentaar gezet.

--- sync/src/main/java/com/capgemini/be/proxy/SyncProxy.java
+++ sync/src/main/java/com/capgemini/be/proxy/SyncProxy.java
@@ -1,25 +1,25 @@
-package com.capgemini.be.proxy;
-
-import com.capgemini.be.lms.model.LeaveRequest;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-
-@Produces(MediaType.APPLICATION_JSON)
-@RegisterRestClient(baseUri = "http://localhost:8081")
-
-
-public interface SyncProxy {
-
-    @POST
-    @Path("/sync")
-    @Consumes(MediaType.APPLICATION_JSON)
-    Response syncLeaveRequest(LeaveRequest leaveRequest);
-
-}
+//package com.capgemini.be.proxy;
+//
+//import com.capgemini.be.lms.model.LeaveRequest;
+//import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+//
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//
+//
+//@Produces(MediaType.APPLICATION_JSON)
+//@RegisterRestClient(baseUri = "http://localhost:8081")
+//
+//
+//public interface SyncProxy {
+//
+//    @POST
+//    @Path("/sync")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    Response syncLeaveRequest(LeaveRequest leaveRequest);
+//
+//}
