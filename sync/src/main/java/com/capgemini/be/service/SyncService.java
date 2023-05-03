c1840e43f471ca602a36419ce90ba9ef72fc4e80
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-05-03 15:23:22 +0200

    sync service aangespast proxy methode uitgehaald, rechtstreeks in de controller

--- sync/src/main/java/com/capgemini/be/service/SyncService.java
+++ sync/src/main/java/com/capgemini/be/service/SyncService.java
@@ -1,14 +1,8 @@
 package com.capgemini.be.service;
 
-import com.capgemini.be.MyLogger;
 import com.capgemini.be.lms.model.LeaveRequest;
-import com.capgemini.be.proxy.SyncProxy;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
 
 @ApplicationScoped
@@ -16,30 +10,13 @@ public class SyncService {
 
     private static final Logger LOGGER = Logger.getLogger(SyncService.class.getName());
 
-    @Inject
-    @RestClient
-    SyncProxy syncProxy;
-
-    public SyncService() {
-        MyLogger.setup();
-    }
-
-    public LeaveRequest createLeaveRequest(LeaveRequest leaveRequest) {
+    public void syncLeaveRequest(LeaveRequest leaveRequest) {
         // create the leave request in the local database
         // ...
 
-        // send the leave request to the sync application
-        try {
-            Response response = syncProxy.syncLeaveRequest(leaveRequest);
-            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-                throw new WebApplicationException("Failed to sync leave request. Response code: " + response.getStatus());
-            }
-            LOGGER.info("Leave request synced successfully: " + leaveRequest.toString());
-        } catch (Exception e) {
-            LOGGER.severe("Failed to sync leave request: " + e.getMessage());
-            throw new WebApplicationException("Failed to sync leave request. Reason: " + e.getMessage());
-        }
+        // send the leave request to the other system
+        // ...
 
-        return leaveRequest;
+        LOGGER.info("Leave request synced successfully: " + leaveRequest.toString());
     }
 }
