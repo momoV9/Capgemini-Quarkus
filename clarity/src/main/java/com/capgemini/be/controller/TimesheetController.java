2ea7027e5b756d1dd971fcefcd31854052f507a6
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-05-05 12:07:41 +0200

    Request body word gelogt

--- clarity/src/main/java/com/capgemini/be/controller/TimesheetController.java
+++ clarity/src/main/java/com/capgemini/be/controller/TimesheetController.java
@@ -1,26 +1,35 @@
 package com.capgemini.be.controller;
 
 import com.capgemini.be.clarity.model.TimeSheet;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Path("/clarity")
 @Produces(MediaType.APPLICATION_JSON)
 public class TimesheetController {
 
+    private static final Logger LOGGER = Logger.getLogger(TimesheetController.class.getName());
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public TimeSheet createTimesheet(TimeSheet timesheet) {
-        // Save the timesheet to the database
-        // ...
-
-        // Map the data from the received timesheet to the Clarity format
-        // and send it to the Clarity API
-
-        return timesheet; // Return the created timesheet as confirmation
+    public TimeSheet createTimesheet(@RequestBody TimeSheet timesheet) {
+        LOGGER.info("Creating timesheet for user: " + timesheet.getUser());
+        List<TimeSheet.TimesheetEntry> entries = timesheet.getEntries();
+        for (int i = 0; i < entries.size(); i++) {
+            TimeSheet.TimesheetEntry entry = entries.get(i);
+            LOGGER.info("Entry " + (i + 1) + ":");
+            LOGGER.info("Date: " + entry.getDate());
+            LOGGER.info("Hours: " + entry.getHours());
+            LOGGER.info("Investment ID: " + entry.getInvestmentId());
+        }
+        return timesheet;
     }
+
 }
