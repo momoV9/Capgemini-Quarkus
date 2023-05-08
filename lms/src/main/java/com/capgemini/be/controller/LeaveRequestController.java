d2f9a9b5077b979599b3998bc7a1a3b32c0b6f89
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-05-08 15:14:45 +0200

    Controllers aangepast fout opgelost lms stuurt request naar sync en sync logt het

--- lms/src/main/java/com/capgemini/be/controller/LeaveRequestController.java
+++ lms/src/main/java/com/capgemini/be/controller/LeaveRequestController.java
@@ -1,31 +1,18 @@
 package com.capgemini.be.controller;
 
 import com.capgemini.be.lms.model.LeaveRequest;
-import com.capgemini.be.service.SyncService;
 
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-@Path("/lms")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces
+@Path("/leaves")
 public class LeaveRequestController {
-
-    @Inject
-    SyncService syncService;
-
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createLeaveRequest(LeaveRequest leaveRequest) {
-        try {
-            syncService.syncLeaveRequest(leaveRequest);
-            return Response.ok().build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
+    public void sendToSync(LeaveRequest leaveRequest) {
     }
 }
--- sync/src/main/java/com/capgemini/be/controller/SyncController.java
+++ sync/src/main/java/com/capgemini/be/controller/SyncController.java
@@ -1,32 +1,23 @@
 package com.capgemini.be.controller;
 
 import com.capgemini.be.lms.model.LeaveRequest;
-import com.capgemini.be.service.SyncService;
 
-import javax.inject.Inject;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
+@Produces
 @Path("/sync")
-@Produces(MediaType.APPLICATION_JSON)
 public class SyncController {
 
-    @Inject
-    SyncService syncService;
+    private static final Logger LOGGER = Logger.getLogger(SyncController.class.getName());
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response syncLeaveRequest(LeaveRequest leaveRequest) {
-        try {
-            syncService.syncLeaveRequest(leaveRequest);
-            return Response.ok().build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
+    public void logRequest(LeaveRequest leaveRequest) {
+        LOGGER.info("Leave request synced successively: " + leaveRequest);
     }
 }
-
