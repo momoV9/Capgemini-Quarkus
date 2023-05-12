ad8942e587759c310751a73d4ff351968f7075bf
abdahman <abdellah.dahmani@student.thomasmore.be>
2023-05-12 12:49:59 +0200

    tests bijgewerkt beforeach toch weg gehaald

--- sync/src/test/java/com/capgemini/be/mapper/TimesheetMapperTest.java
+++ sync/src/test/java/com/capgemini/be/mapper/TimesheetMapperTest.java
@@ -2,37 +2,25 @@ package com.capgemini.be.mapper;
 
 import com.capgemini.be.clarity.model.TimeSheet;
 import com.capgemini.be.lms.model.LeaveRequest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.wildfly.common.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class TimesheetMapperTest {
     private LeaveRequest leaveRequest;
-    private TimesheetMapper mapper;
+    private final TimesheetMapper mapper = new TimesheetMapper();
 
-    @BeforeEach
-    public void setup() {
-        // Arrange
+    @Test
+    void testMap() {
+        // Arange
         leaveRequest = new LeaveRequest();
         leaveRequest.setLocalEmployeeId(123456);
         leaveRequest.setStartDate(LocalDate.of(2023, 5, 10));
         leaveRequest.setStartHours(1);
-
-
-        mapper = new TimesheetMapper();
-    }
-
-    @Test
-    void testMap() {
-        // Arrange
-        leaveRequest.setLocalEmployeeId(123456);
-        leaveRequest.setStartDate(LocalDate.of(2023, 5, 10));
-        leaveRequest.setStartHours(1);
-        mapper = new TimesheetMapper();
 
         // Act
         TimeSheet timeSheet = mapper.map(leaveRequest);
@@ -45,16 +33,15 @@ public class TimesheetMapperTest {
         TimeSheet.TimesheetEntry entry = timeSheet.getEntries().get(0);
         assertEquals(LocalDate.of(2023, 5, 10), entry.getDate());
         assertEquals(8, entry.getHours());
-        assertEquals("investmentid123", entry.getInvestmentId());
     }
 
     @Test
     void testMapSingleEntry() {
-        // Arrange
+        // Arange
+        leaveRequest = new LeaveRequest();
         leaveRequest.setLocalEmployeeId(12345);
         leaveRequest.setStartDate(LocalDate.now());
         leaveRequest.setStartHours(8);
-        mapper = new TimesheetMapper();
 
         // Act
         TimeSheet timeSheet = mapper.map(leaveRequest);
@@ -66,16 +53,15 @@ public class TimesheetMapperTest {
         TimeSheet.TimesheetEntry entry = timeSheet.getEntries().get(0);
         assertEquals(leaveRequest.getStartDate(), entry.getDate());
         assertEquals(8 * 8, entry.getHours());
-        assertEquals("investmentid123", entry.getInvestmentId());
     }
 
     @Test
     void testMapHalfDayEntry() {
-        // Arrange
+        // Arange
+        leaveRequest = new LeaveRequest();
         leaveRequest.setLocalEmployeeId(123456);
         leaveRequest.setStartDate(LocalDate.of(2023, 5, 12));
         leaveRequest.setStartHours(4);
-        mapper = new TimesheetMapper();
 
         // Act
         TimeSheet timeSheet = mapper.map(leaveRequest);
@@ -87,6 +73,5 @@ public class TimesheetMapperTest {
         TimeSheet.TimesheetEntry entry = timeSheet.getEntries().get(0);
         assertEquals(leaveRequest.getStartDate(), entry.getDate());
         assertEquals(4 * 8, entry.getHours());
-        assertEquals("investmentid123", entry.getInvestmentId());
     }
 }
