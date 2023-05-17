45644ef77626cfb29dae7618f37f48210bfe31ab
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-05-17 13:06:55 +0200

    ticket 10 klaar

--- sync/src/main/java/com/capgemini/be/controller/MetricsController.java
+++ sync/src/main/java/com/capgemini/be/controller/MetricsController.java
@@ -1,4 +1,19 @@
 package com.capgemini.be.controller;
 
+import com.capgemini.be.lms.model.LeaveRequest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/metrics")
 public class MetricsController {
+
+    @GET
+    public List<LeaveRequest> getMetrics() {
+        return null;
+    }
 }
