2012f36034b6edcad6c0c9ee3d1ec95b71045065
momov9 <r0905334@student.thomasmore.be>
2023-05-17 17:31:49 +0200

    added Metrics table page but still needs to read startdate, enddate etc properly

---
+++ sync/src/main/resources/META-INF/resources/dashboard.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:8081/metrics')
+        .then(response => response.json())
+        .then(data => {
+            const dashboardDataElement = document.getElementById('dashboard-data');
+            data.forEach(entry => {
+                const row = document.createElement('tr');
+                row.innerHTML =
+                    <td>${entry.user}</td>
+                <td>${entry.startDate} - ${entry.endDate}</td>
+                <td>${entry.leaveType}</td>
+                <td>${entry.hours}</td>
+                ;
+                dashboardDataElement.appendChild(row);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+});
