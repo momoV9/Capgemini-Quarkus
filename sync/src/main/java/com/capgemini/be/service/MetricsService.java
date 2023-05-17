e1f98c696923ed7e1a674f83cfd70a6cb109a84b
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-05-17 15:20:17 +0200

    MetricsService aangemaakt met addMetricsEntry methode

--- sync/src/main/java/com/capgemini/be/service/MetricsService.java
+++ sync/src/main/java/com/capgemini/be/service/MetricsService.java
@@ -1,4 +1,27 @@
 package com.capgemini.be.service;
 
+import com.capgemini.be.lms.model.LeaveRequest;
+import com.capgemini.be.model.MetricsEntry;
+import com.capgemini.be.repository.MetricsRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
 public class MetricsService {
+
+    @Inject
+    MetricsRepository metricsRepository;
+
+    public void addMetricsEntry(LeaveRequest leaveRequest) {
+        MetricsEntry entry = new MetricsEntry();
+        entry.setUser(String.valueOf(leaveRequest.getLocalEmployeeId()));
+        entry.setStartDate(leaveRequest.getStartDate());
+        entry.setEndDate(leaveRequest.getEndDate());
+        entry.setLeaveType(leaveRequest.getLeaveType());
+        entry.setHours(leaveRequest.getTotalHours());
+
+        metricsRepository.addEntry(entry);
+    }
 }
+
