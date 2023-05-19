127c585e136f935d5bb0c0c56e15f2d4f332400f
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-05-19 16:54:38 +0200

    data in memory

--- sync/src/main/java/com/capgemini/be/repository/MetricsRepository.java
+++ sync/src/main/java/com/capgemini/be/repository/MetricsRepository.java
@@ -1,22 +1,22 @@
 package com.capgemini.be.repository;
 
+import com.capgemini.be.lms.model.LeaveType;
 import com.capgemini.be.model.MetricsEntry;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @ApplicationScoped
 public class MetricsRepository {
-    private List<MetricsEntry> metricsCollection;
+    private final List<MetricsEntry> metricsCollection;
 
     public MetricsRepository() {
         metricsCollection = new ArrayList<>();
-    }
-
-    public void addEntry(MetricsEntry entry) {
-        metricsCollection.add(entry);
+        metricsCollection.add(new MetricsEntry("John Doe", LocalDate.parse("2023-05-10"), LocalDate.parse("2023-05-12"), LeaveType.VACATION, 24));
+        metricsCollection.add(new MetricsEntry("Jane Smith", LocalDate.parse("2023-05-11"), LocalDate.parse("2023-05-13"), LeaveType.PARENTAL, 16));
     }
 
     public List<MetricsEntry> getAllEntries() {
