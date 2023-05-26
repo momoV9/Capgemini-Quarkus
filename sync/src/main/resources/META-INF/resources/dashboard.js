9421d0fc67123e7f0f7238066c1b2aa2af96803b
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-05-26 20:36:08 +0200

    Js aangepast naar deveextreme cdn

--- sync/src/main/resources/META-INF/resources/dashboard.js
+++ sync/src/main/resources/META-INF/resources/dashboard.js
@@ -4,29 +4,27 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             const container = document.getElementById('dashboard-container');
 
-            const hotInstance = new Handsontable(container, {
-                data: data,
+            const gridOptions = {
+                dataSource: data,
                 columns: [
-                    { data: 'user' },
-                    { data: 'leaveType' },
-                    { data: 'startDate' },
-                    { data: 'endDate' },
-                    { data: 'hours' }
+                    { dataField: 'user', caption: 'User' },
+                    { dataField: 'leaveType', caption: 'Leave Type' },
+                    { dataField: 'startDate', caption: 'Start Date' },
+                    { dataField: 'endDate', caption: 'End Date' },
+                    { dataField: 'hours', caption: 'Hours' }
                 ],
-                columnSorting: true,
-                manualRowMove: true,
-                pagination: true,
-                maxRows: 120,
-                rowHeaders: true,
-                colHeaders: ['User', 'Leave Type', 'Start Date', 'End Date', 'Hours'],
-                contextMenu: true,
-                licenseKey: 'non-commercial-and-evaluation'
-            });
+                columnAutoWidth: true,
+                showBorders: true,
+                editing: {
+                    allowDeleting: true
+                }
+            };
 
-            hotInstance.addHook('afterChange', function(changes) {
-                console.log('Data is gewijzigd:', changes);
+            const dashboardContainer = document.getElementById('dashboard-container');
+            const dataGrid = new DevExpress.ui.dxDataGrid(dashboardContainer, gridOptions);
+            dataGrid.on('cellValueChanged', function(e) {
+                console.log('Data is gewijzigd:', e);
             });
-
         })
         .catch(error => console.error('Error:', error));
 });
